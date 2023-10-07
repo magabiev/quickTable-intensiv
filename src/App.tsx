13c0520d6fefb51a8fb18ffdd1a0e9f7e4767879
magabiev <magomed.ebiev@gost-group.com>
2023-10-07 14:28:21 +0300

    actions error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 function App() {
-
+  const s = '';
   return (
-    <div>QuickTable Main Page</div>
-  )
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+      adipisci consequatur culpa cumque delectus illum, incidunt laborum minus
+      neque nulla officiis, perspiciatis porro quod ratione repudiandae tempora
+      voluptatem! Unde, voluptates!
+    </div>
+  );
 }
 
-export default App
+export default App;
