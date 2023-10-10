6ddee147b643f67c5f9a007bed126fd959e70362
magabiev <magomed.ebiev@gost-group.com>
2023-10-10 20:23:43 +0300

    new main yaml

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 function App() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-      adipisci consequatur culpa cumque delectus illum, incidunt laborum minus
-      neque nulla officiis, perspiciatis porro quod ratione repudiandae tempora
-      voluptatem! Unde, voluptates!
-    </div>
-  );
+  return <div>Test text</div>;
 }
 
 export default App;
